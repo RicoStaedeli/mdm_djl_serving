fcda8a63bbd4eada3d3fac56c4ccc359e3def055
Rico Staedeli ZHAW <staedric@students.zhaw.ch>
2023-05-25 23:13:37 +0200

    first commit

---
+++ src/main/java/com/rico/mdm/servingexample/ServingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rico.mdm.servingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rico/mdm/servingexample/ServingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rico.mdm.servingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
